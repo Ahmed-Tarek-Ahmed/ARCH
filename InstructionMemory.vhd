16cabaa011f8963e8d3b8dce6e648b0a0462eb43
HACKAMAN1337 <49288530+HACKAMAN1337@users.noreply.github.com>
2020-05-25 08:51:58 +0200

    integrattimn

--- InstructionMemory.vhd
+++ InstructionMemory.vhd
@@ -2,16 +2,16 @@ LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.numeric_std.all;
 
-ENTITY ram IS
+ENTITY instmem IS
 	PORT(
 		address : IN  std_logic_vector(10 DOWNTO 0);
 		dataout : OUT std_logic_vector(31 DOWNTO 0));
-END ENTITY ram;
+END ENTITY instmem;
 
-ARCHITECTURE syncrama OF ram IS
+ARCHITECTURE instmem1 OF instmem IS
 
 	TYPE ram_type IS ARRAY(0 TO 2047) OF std_logic_vector(15 DOWNTO 0);
 	SIGNAL ram : ram_type ;
 BEGIN
 	dataout <= ram(to_integer(unsigned(address))) & ram(to_integer(unsigned(address))+1);
-END syncrama;
+END instmem1;
