1800ca550b485a050229b9725b9b5141edb7f167
HACKAMAN1337 <49288530+HACKAMAN1337@users.noreply.github.com>
2020-05-25 08:43:25 +0200

    Update Integration.vhd

--- Integration.vhd
+++ Integration.vhd
@@ -3,11 +3,13 @@ USE IEEE.std_logic_1164.all;
 
 Entity processor IS 
 port(
+	input :IN std_logic_vector (31 downto 0);
+	output :OUT std_logic_vector (31 downto 0);
+	reset,intr :IN std_logic;
 );
 END ENTITY;
 
 Architecture p1 OF processor IS
-
 begin
 
 END Architecture;
