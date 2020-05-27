01a04893c62eec7b591e901e3ada5720532c37a0
HACKAMAN1337 <49288530+HACKAMAN1337@users.noreply.github.com>
2020-05-27 21:54:22 +0200

    7alawa

---
+++ buffer.vhd
@@ -0,0 +1,27 @@
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.all;
+
+Entity G_Register1 IS
+ Generic (n:integer:=32);
+port(
+  D:IN std_logic_vector(n-1 downto 0);
+  Q:OUT std_logic_vector(n-1 downto 0):=(others =>'0');
+  clk,rst,enable:IN std_Logic
+);
+END ENTITY;
+
+Architecture a_G_Register OF G_Register1 IS
+
+begin
+
+PROCESS(clk,rst)
+begin
+  IF(rst='1') THEN
+  Q <=(others=>'0');
+ELSIF rising_edge(clk) THEN
+  if(enable='1') then   
+  Q<=D;
+END IF;
+END IF;
+END PROCESS;
+END Architecture;
