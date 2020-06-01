77ca73e7ed68c08424539496ac861ed50d69f6c7
HACKAMAN1337 <49288530+HACKAMAN1337@users.noreply.github.com>
2020-06-01 20:29:08 +0200

    Update ForwardingUnit.vhd

--- ForwardingUnit.vhd
+++ ForwardingUnit.vhd
@@ -15,7 +15,7 @@ Architecture a_Forward OF Forward IS
 PROCESS(Rsrc1,Rsrc2,RdstMem,RdstWB,WB_SMem,WB_SWB,outMem,outWB)
   begin
   IF (Rsrc1=RdstMem) then
-    IF (outMem='1' or WB_SMem= "01") then
+    IF (WB_SMem= "01") then
         Rsrc1MUX<= "010";
         --Rsrc2MUX<= "001";
     ELSIF (WB_SMem(1)='1')then
@@ -23,7 +23,7 @@ PROCESS(Rsrc1,Rsrc2,RdstMem,RdstWB,WB_SMem,WB_SWB,outMem,outWB)
         --Rsrc2MUX<= "001";
     ELSE
     IF (Rsrc1=RdstWB ) then
-    IF (outWB='1' or WB_SWB= "01") then
+    IF (WB_SWB= "01") then
         Rsrc1MUX<= "011";
         --Rsrc2MUX<= "001";
     ELSIF (WB_Swb(1)='1')then
@@ -32,10 +32,12 @@ PROCESS(Rsrc1,Rsrc2,RdstMem,RdstWB,WB_SMem,WB_SWB,outMem,outWB)
     ELSE
       Rsrc1MUX<= "001";
     END IF;
+         ELSE
+      Rsrc1MUX<= "001";
     END IF;
     End if;
   ELSIF (Rsrc1=RdstWB ) then
-    IF (outWB='1' or WB_SWB= "01") then
+    IF (WB_SWB= "01") then
         Rsrc1MUX<= "011";
         --Rsrc2MUX<= "001";
     ELSIF (WB_Swb(1)='1')then
@@ -47,7 +49,7 @@ PROCESS(Rsrc1,Rsrc2,RdstMem,RdstWB,WB_SMem,WB_SWB,outMem,outWB)
    ELSE  Rsrc1MUX<= "001";
   END IF;
   IF (Rsrc2=RdstMem ) then
-    IF (outMem='1' or WB_SMem= "01") then
+    IF (WB_SMem= "01") then
         Rsrc2MUX<= "010";
         --Rsrc1MUX<= "001";
     ELSIF (WB_SMem(1)='1')then
@@ -55,7 +57,7 @@ PROCESS(Rsrc1,Rsrc2,RdstMem,RdstWB,WB_SMem,WB_SWB,outMem,outWB)
         --Rsrc1MUX<= "001";
     ELSE
       IF (Rsrc2=RdstWB ) then
-    IF (outWB='1' or WB_SWB= "01") then
+    IF (WB_SWB= "01") then
         Rsrc2MUX<= "011";
         --Rsrc1MUX<= "001";
     ELSIF (WB_Swb(1)='1')then
@@ -64,11 +66,13 @@ PROCESS(Rsrc1,Rsrc2,RdstMem,RdstWB,WB_SMem,WB_SWB,outMem,outWB)
     ELSE
       Rsrc2MUX<= "001";
     END IF;
+	else
+   Rsrc2MUX<= "001";
     END IF;
     END IF;
     
   ELSIF (Rsrc2=RdstWB ) then
-    IF (outWB='1' or WB_SWB= "01") then
+    IF (WB_SWB= "01") then
         Rsrc2MUX<= "011";
         --Rsrc1MUX<= "001";
     ELSIF (WB_Swb(1)='1')then
